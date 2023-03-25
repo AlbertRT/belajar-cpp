b9d7ef7b69f17b604874bdcdc463ddc60cbe4d7e
Albert <87812438+AlbertRT@users.noreply.github.com>
2023-03-25 09:07:17 +0700

    Variable

---
+++ Variable/Variable.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    long long LONG;
+    int INTEGER;
+    string STRING;
+    float FLOAT;
+    bool BOOLEAN;
+
+    LONG = 10000000000000;
+    INTEGER = 10;
+    STRING = "Ini adalah string";
+    FLOAT = 1.991;
+    BOOLEAN = true;
+    
+    cout << LONG << endl;
+    cout << INTEGER << endl;
+    cout << STRING << endl;
+    cout << FLOAT << endl;
+    cout << BOOLEAN << endl;
+
+    return 0;
+}
