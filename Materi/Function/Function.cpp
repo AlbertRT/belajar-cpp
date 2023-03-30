06a6cae6607c0658bbd90edd975f7f12c356a620
Albert <87812438+AlbertRT@users.noreply.github.com>
2023-03-30 18:12:23 +0700

    Function (cmath lib)

---
+++ Materi/Function/Function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Masukan jumlah angka: ";
+    cin >> n;
+
+    cout << "sqrt(" << n << ") : " << sqrt(n) << endl;
+    cout << "log10(" << n << ") : " << log10(n) << endl;
+
+    return  0;
+}
